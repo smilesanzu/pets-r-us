7886cbb4e426970cc14ac4128a76456bf1b08b07
Janis <gonzalezjanisx@gmail.com>
2023-02-11 14:14:00 -0800

    added comments.

--- models/customer.js
+++ models/customer.js
@@ -1,6 +1,7 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// unique and required
 let customerSchema = new Schema({
     customerId: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true }
