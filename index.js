221d514762c70bc0caf6a60e03ca2625834147ff
Janis <gonzalezjanisx@gmail.com>
2023-03-03 13:33:04 -0800

    updated.

--- index.js
+++ index.js
@@ -11,7 +11,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const Customer = require('./models/customer');
+const Customer = require('./models/customer.js');
 // express variable
 const app = express();
 
